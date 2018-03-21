48afe5e28d4f4c249b30cd9b65adbbb24d1aea3a
Cameron84311 <34030267+Cameron84311@users.noreply.github.com>
2018-03-21 09:58:46 +0000

    Added ResetLevel Script

---
+++ Assets/Scripts/ResetLevel.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public static class ResetLevel : MonoBehaviour {
+	
+
+	static void RestartLevel(){
+		Scene scene = SceneManager.GetActiveScene ();
+		SceneManager.LoadScene (scene.name);
+
+		
+
+
+	}
+
+
+
+
+
+
+
+
+
+}
+
+ 
