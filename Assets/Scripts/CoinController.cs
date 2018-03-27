2752ab4ce64d5e554e6f5524ab4b0efde03c5073
Ryan <xryanjw@gmail.com>
2018-03-27 16:24:57 +0100

    new coin script start

---
+++ Assets/Scripts/CoinController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class CoinController {
+
+    public static GameObject[] coins;
+
+	static void Start () {
+        coins = GameObject.FindGameObjectsWithTag("Coin");
+	}
+
+    static void CollectCoin()
+    {
+
+    }
+}
