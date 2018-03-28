4eb0f2cc91d0a03f0da19472ec7a12f8a3f4f56b
RobertTemple <roberttemple2001@gmail.com>
2018-03-28 09:40:18 +0100

    Timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+	public float timer; //This float will store the time
+
+
+	void Update () {
+		
+		timer += Time.deltaTime;
+
+		GetComponent<TextMesh> ().text = Mathf.Floor (timer).ToString ();
+	
+	}
+}
